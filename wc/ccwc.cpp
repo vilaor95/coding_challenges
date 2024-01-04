712d3e738b8168cd3d0503aa862c65a09c18c829
vlo <vilaor@teleco.upv.es>
2024-01-04 18:40:27 +0100

    count utf8 characters

--- wc/ccwc.cpp
+++ wc/ccwc.cpp
@@ -8,21 +8,22 @@ using namespace std;
 #include <fstream>
 
 static void help(void);
-static size_t count_bytes(ifstream &infile);
-static size_t count_words(ifstream &infile);
-static size_t count_lines(ifstream &infile);
+static size_t count_bytes(istream &infile);
+static size_t count_words(istream &infile);
+static size_t count_lines(istream &infile);
+static size_t count_characters(istream &infile);
 
 int main(int argc, char* argv[])
 {
-	bool cflag = false, lflag = false, wflag = false;
+	bool cflag = false, lflag = false, wflag = false, mflag = false;
 	string input_filename = "";
 	int c;
 
-	size_t number_of_bytes, number_of_lines, number_of_words;
+	size_t number_of_bytes, number_of_lines, number_of_words, number_of_characters;
 
 	ifstream input_file;
 
-	while((c = getopt(argc, argv, "clw")) != -1) {
+	while((c = getopt(argc, argv, "clwm")) != -1) {
 		switch(c) {
 			case 'c':
 				cflag = true;
@@ -32,6 +33,9 @@ int main(int argc, char* argv[])
 				break;
 			case 'w':
 				wflag = true;
+				break;
+			case 'm':
+				mflag = true;
 				break;
 			case '?':
 			case ':':
@@ -71,6 +75,12 @@ int main(int argc, char* argv[])
 		cout << number_of_words << "\t";
 	}
 
+	if (mflag) {
+		number_of_characters = count_characters(input_file);
+
+		cout << number_of_characters << "\t";
+	}
+
 	cout << input_filename;
 
 	input_file.close();
@@ -82,7 +92,7 @@ static void help() {
 	exit(1);
 }
 
-static size_t count_bytes(ifstream &infile) {
+static size_t count_bytes(istream &infile) {
 	size_t bytes = 0;
 
 	infile.seekg(0, ios::beg);
@@ -92,7 +102,7 @@ static size_t count_bytes(ifstream &infile) {
 	return bytes;
 }
 
-static size_t count_lines(ifstream &infile) {
+static size_t count_lines(istream &infile) {
 	size_t lines = 0;	
 
 	infile.seekg(0, ios::beg);
@@ -107,7 +117,7 @@ static size_t count_lines(ifstream &infile) {
 	return lines;
 }
 
-static size_t count_words(ifstream &infile) {
+static size_t count_words(istream &infile) {
 	size_t words = 0;
 
 	infile.seekg(0, ios::beg);
@@ -119,4 +129,42 @@ static size_t count_words(ifstream &infile) {
 	}
 
 	return words;
+}
+
+static size_t count_characters(istream &infile) {
+	size_t characters = 0;
+
+	infile.seekg(0, ios::beg);
+
+	char ch;
+	while(infile >> noskipws >> ch)
+	{
+		if ((ch & 0x80) == 0x00)
+		{
+			characters++;
+		}
+		else if ((ch & 0xC0) == 0xC0)
+		{
+			characters++;
+			infile >> ch;
+		}
+		else if ((ch & 0xE0) == 0xE0)
+		{
+			characters++;
+			infile >> ch;
+			infile >> ch;
+		}
+		else if ((ch & 0xF0) == 0xF0)
+		{
+			characters++;
+			infile >> ch;
+			infile >> ch;
+			infile >> ch;
+		}
+		else {
+			//error
+		}
+	}
+
+	return characters;
 }
